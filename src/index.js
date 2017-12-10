27f47705f36f29d655c5db19db2563e1b35e4976
Jonathan Chen <chaochen42@gmail.com>
2017-12-11 11:29:40 +1300

    six world

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const element = React.createElement('h1', { className: 'greeting' }, 'Hello, Fifth World');
+const element = React.createElement('h1', { className: 'greeting' }, 'Hello, sixth World');
 ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
 
