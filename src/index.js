1e0ba2c266196a1049a6fafb12206088a3e53ba9
Jonathan Chen <chaochen42@gmail.com>
2017-12-11 11:17:42 +1300

    update index.js
    third world

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const element = React.createElement('h1', { className: 'greeting' }, 'Hello, Second World');
+const element = React.createElement('h1', { className: 'greeting' }, 'Hello, Third World');
 ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
 
