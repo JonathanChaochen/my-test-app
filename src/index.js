79154a7898d2379c34a05c7c1939c21b512be84b
Jonathan Chen <chaochen42@gmail.com>
2017-12-11 16:54:14 +1300

    ten try

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const element = React.createElement('h1', { className: 'greeting' }, 'Hello, eight World');
+const element = React.createElement('h1', { className: 'greeting' }, 'Hello, ten World');
 ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
 
